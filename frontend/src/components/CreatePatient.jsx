cb62dc1e3988de9c51a6c2c8fd06094dad9820c8
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-05-11 22:26:22 +1000

    started create patient function

--- frontend/src/components/CreatePatient.jsx
+++ frontend/src/components/CreatePatient.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 function CreatePatient() {
     return (
-        <div>
-            CREATE PATIENT
+        <div className= "container">
+        <h1> Create Patient Page </h1>
+        <br></br>
+        <form>
+            <div className="row">
+                <div className="col-sm">
+                    <div className='form-group'>
+                        <input autoComplete="off" className='form-control' placeholder="Title"></input>
+                    </div>
+                    <div className='form-group'>
+                        <input autoComplete="off" className='form-control' placeholder="Last Name"></input>
+                    </div>
+
+                    <div className='form-group'>
+                        <input autoComplete="off" className='form-control' placeholder="First Name"></input>
+                    </div>
+                </div>
+            </div>
+            <Button variant="danger">SAVE</Button>
+
+        </form>
         </div>
     )
 }
