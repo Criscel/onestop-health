ed688b5f558982ef0448b86b33bfdcc9b829f889
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-05-11 23:24:11 +1000

    created route

---
+++ routes/patientRoute.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const Patient = require("../models/patientModel");
+
+//configure the route to get and send data from database
+
+module.exports = router;
