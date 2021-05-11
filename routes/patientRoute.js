c7329cde02b63d5a59336460cc02076e6cce0b70
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-05-11 23:31:50 +1000

    updated patientRoute

--- routes/patientRoute.js
+++ routes/patientRoute.js
@@ -3,5 +3,17 @@ const router = express.Router();
 const Patient = require("../models/patientModel");
 
 //configure the route to get and send data from database
+router.route("/create").post((req, res) => {
+    const title = req.body.title;
+    const lastname = req.body.lastname;
+    const firstname = req.body.firstname;
+    const newPatient = new Patient({
+        title,
+        lastname,
+        firstname
+    });
+
+    newPatient.save();
+});
 
 module.exports = router;
