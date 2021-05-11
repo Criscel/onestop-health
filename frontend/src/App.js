b2883c4592fb722231e23b688ccbeac24cfa024d
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-05-11 21:42:04 +1000

    added navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import {} from "react-router-dom";
 
 
 function App() {
   return (
     <div>
       <h1> REACT HERE </h1>
-    </div>
+    </div> 
   );
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Link} from "react-router-dom"
+
+
+function Navbar() {
+    return <nav className="navbar bg-light container">
+        <h4><Link className="link" to="/">Home</Link></h4>
+        <h4><Link className="link" to="/lists">Patient List</Link></h4>
+        <h4><Link className="link" to="/create">Create Patient</Link></h4>
+    </nav>
+} 
+
+export default Navbar; 
