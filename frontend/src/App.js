f197a125410099e1cc8704c6d98e98b825a2ce9c
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-05-11 21:56:21 +1000

    navbar working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
-import {} from "react-router-dom";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <h1> REACT HERE </h1>
-    </div> 
+      <Router>
+        <Navbar />
+
+        <Route path="/" exact>
+          HOME
+        </Route>
+
+        <Route path="/lists">
+          PATIENT LIST
+        </Route>
+
+        <Route path="/create">
+          CREATE PATIENT
+        </Route>
+        
+      </Router>
+    </div>
   );
 }
 
