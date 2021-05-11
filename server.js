8b42d80a045c60b03f74b24df9cacb87b071df55
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-05-11 23:18:41 +1000

    connected mongoose

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 //connect to moongose
+mongoose.connect("mongodb+srv://dbUser:Admin123@celcluster.p4l5f.mongodb.net/medical_records_db", { useNewUrlParser: true, useUnifiedTopology: true });
 
 //require route
 
