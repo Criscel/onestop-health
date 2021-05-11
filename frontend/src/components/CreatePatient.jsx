e5a630f6d872e4923bb561051048b1d5db0e94e1
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-05-11 22:43:14 +1000

    handle events working

--- frontend/src/components/CreatePatient.jsx
+++ frontend/src/components/CreatePatient.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 
 function CreatePatient() {
+    const [input, setInput] = useState({
+        title: '',
+        lastname: '',
+        firstname: ''
+    })
+
+    function handleChange(event) {
+        // console.log(evet.target)
+        const { name, value } = event.target;
+
+        setInput(prevInput => {
+            return {
+                ...prevInput,
+                [name]: value
+            }
+        })
+    }
+
+    function handleClick(event) {
+        event.preventDefault();
+        console.log(input);
+    }
     return (
         <div className= "container">
         <h1> Create Patient Page </h1>
@@ -10,18 +32,18 @@ function CreatePatient() {
             <div className="row">
                 <div className="col-sm">
                     <div className='form-group'>
-                        <input autoComplete="off" className='form-control' placeholder="Title"></input>
+                        <input onChange={handleChange} name="title" value={input.title} autoComplete="off" className='form-control' placeholder="Title"></input>
                     </div>
                     <div className='form-group'>
-                        <input autoComplete="off" className='form-control' placeholder="Last Name"></input>
+                        <input onChange={handleChange} name="lastname" value={input.lastname} autoComplete="off" className='form-control' placeholder="Last Name"></input>
                     </div>
 
                     <div className='form-group'>
-                        <input autoComplete="off" className='form-control' placeholder="First Name"></input>
+                        <input onChange={handleChange} name="firstname" value={input.firstname} autoComplete="off" className='form-control' placeholder="First Name"></input>
                     </div>
                 </div>
             </div>
-            <Button variant="danger">SAVE</Button>
+            <Button onClick={handleClick} variant="danger">SAVE</Button>
 
         </form>
         </div>
