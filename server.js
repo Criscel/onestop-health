b06b27c324ead5709028095b72c839d6fef3a559
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-05-18 17:46:52 +1000

    deployment

--- server.js
+++ server.js
@@ -17,15 +17,13 @@ mongoose.connect(
     }
   );
   
-  if (process.env.NODE_ENV === 'production') {
-    // Express will serve up production assets
-    app.use(express.static('frontend/build'));
+  if(process.env.NODE.ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'frontend', 'build')));
   
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
     });
+    
   }
 
 
