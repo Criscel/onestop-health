db5cb9df6f613ba05517d5421cb3e7ee83d76afd
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-05-19 00:03:12 +1000

    deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+// const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
   <Auth0Provider 
-    domain={domain}
-    clientId={clientId}
+    domain="lecsirc.au.auth0.com"
+    clientId="ToZa6EpmJG5gFfJ4MzUwWPwqJqaXpHmO"
     redirectUri={window.location.origin}>
     <App />
   </Auth0Provider>,
