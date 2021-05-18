ef426715c189750bbcc8f29bdaa3e694388cbc9a
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-05-18 18:29:58 +1000

    deployment

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(
     }
   );
   
-  if(process.env.NODE.ENV === 'production') {
+  if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'frontend', 'build')));
   
     app.get('*', (req, res) => {
